ad4d645bb05d5c8e5640c4fe29b9947d073809ce
Princess Sampson <ps@kingonomics.com>
2016-09-08 23:31:04 -0400

    Initial commit

---
+++ Dossiers/Dossier.swift
@@ -0,0 +1,26 @@
+//
+//  Dossier.swift
+//  Dossiers
+//
+//  Created by Princess Sampson on 9/8/16.
+//  Copyright © 2016 Princess Sampson. All rights reserved.
+//
+
+import Foundation
+
+class Dossier {
+    var name: String
+    var lastKnownLocation: String
+    var knownAssociates: [String]
+    var occupation: String
+    var languagesSpoken: [String]
+    
+    
+    required init(withName name:String, lastKnownLocation:String, knownAssociates:[String],  occupation: String, languagesSpoken:[String]) {
+        self.name = name
+        self.lastKnownLocation = lastKnownLocation
+        self.knownAssociates = knownAssociates
+        self.occupation = occupation
+        self.languagesSpoken = languagesSpoken
+    }
+}
